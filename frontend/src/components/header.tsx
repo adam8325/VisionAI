547cbd97c4d93bd5d443b25c4709e9b50a29915a
Adam Eleraky <adam_eleraky@hotmail.com>
2025-10-22 09:41:28 +0200

    Responsive design implemented

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -6,15 +6,15 @@ export default function Header() {
       <header className="pt-10 w-4/6">
         <div className="flex flex-col items-center justify-center gap-10">
           <div className="flex items-center gap-3">
-            <Sparkles className="w-10 h-10 text-sky-100"/>
+            <Sparkles className="sm:w-10 sm:h-10 text-sky-100"/>
             <h1
-                className="text-shadow-sm text-xl sm:text-6xl font-bold bg-gradient-to-r from-indigo-600 via-cyan-500 to-lime-300 bg-clip-text text-transparent"
+                className="text-shadow-sm text-4xl sm:text-6xl font-bold bg-gradient-to-r from-indigo-600 via-cyan-500 to-lime-300 bg-clip-text text-transparent"
               >
                 VisionAI
               </h1>
           </div>
-          <div className="text-center">              
-              <h2 className="font-bold sm:text-xl">
+          <div className="text-center">    
+              <h2 className="font-bold text-md sm:text-xl">
                 Opdag AI-muligheder til din virksomhed. Indtast din virksomheds webadresse for at modtage skræddersyede AI-optimeringsløsninger.
               </h2>
           </div> 
--- frontend/src/components/input.tsx
+++ frontend/src/components/input.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Sparkles, Link2, Search, Zap, TrendingUp, Rocket } from "lucide-react";
+import { Link2, Search, Zap, TrendingUp, Rocket } from "lucide-react";
 
 type Recommendation = {
   title: string;
@@ -104,20 +104,20 @@ export default function Input() {
     {/* URL input + knap */}
     <div className="flex items-center w-4/6 mx-auto justify-between gap-2 bg-gray-900 p-2 rounded-lg">
       <div className="relative w-full">
-        <Link2 className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500 w-5 h-5" />
+        <Link2 className="absolute left-1.5 sm:left-3 top-1/2 -translate-y-1/2 text-gray-500 w-4 h-4 sm:w-5 sm:h-5" />
         <input
           type="text"
           value={url}
           onChange={(e) => setUrl(e.target.value)}
           placeholder="https://www.example.com"
-          className="bg-[hsl(222_47%_5%)] rounded-md pl-12 pr-3 py-2 w-full focus:border-lime-500 focus:ring-1 focus:ring-blue-500 outline-none"
+          className="bg-[hsl(222_47%_5%)] rounded-md pl-8 sm:pl-12 pr-3 py-1 sm:py-2 w-full focus:border-lime-500 focus:ring-1 focus:ring-blue-500 outline-none"
         />
       </div>
 
       <button
         onClick={handleSubmit}
         disabled={!url || isLoading}
-        className={`flex items-center justify-center gap-2 font-semibold py-2 px-3 rounded-md
+        className={`flex items-center text-sm sm:text-lg justify-center gap-2 font-semibold py-1 px-2 sm:py-2 sm:px-3 rounded-md
           ${isLoading || !url
             ? "bg-gray-600 text-gray-500 cursor-not-allowed"
             : "cursor-pointer bg-linear-to-r from-cyan-100 via-blue-200 to-indigo-300 text-gray-900 hover:from-cyan-300 to-emerald-900"
@@ -137,18 +137,18 @@ export default function Input() {
         <div className="w-4/6 mx-auto">
           {summary && (
             <div className="flex flex-col gap-6 bg-gray-900 border border-stone-800 p-4 rounded-lg shadow-sm">
-              <div className="flex items-center justify-between">
-                <h3 className="font-bold text-xl mb-1">{name}</h3>
-                <p className="bg-blue-200 text-gray-900 text-sm rounded-2xl py-1 px-2.5">{branch}</p>
+              <div className="flex gap-2 flex-row items-center justify-between">
+                <h3 className="font-bold text-md sm:text-xl mb-1">{name}</h3>
+                <label className="bg-blue-200 text-gray-900 text-[10px] sm:text-sm rounded-2xl py-1 px-2.5">{branch}</label>
               </div>              
-              <p className="font-semibold">{summary}</p>
+              <p className="font-semibold text-sm sm:text-md">{summary}</p>
             </div>
           )}
         </div>
 
         {/* Roadmap titel */}
         {isDone && (
-          <div className="flex flex-col items-center justify-center w-4/6 mx-auto gap-4">
+          <div className="flex flex-col items-center text-center justify-center w-4/6 mx-auto gap-4">
             <h3 className="text-shadow-sm text-xl sm:text-3xl font-bold bg-gradient-to-r from-indigo-600 via-cyan-500 to-lime-300 bg-clip-text text-transparent">
               AI Implementerings roadmap
             </h3>
@@ -159,11 +159,11 @@ export default function Input() {
         )}
 
         {/* Anbefalinger */}
-        <div className="flex items-start justify-center gap-8 w-full h-full">
+        <div className="flex flex-col sm:flex-row items-start justify-center gap-8 mx-auto w-3/4 sm:w-full h-full">
           {recommendations.map((rec, i) => (
             <div
               key={i}
-              className="group flex flex-col gap-4 bg-gray-900 p-4 border border-stone-800 rounded-lg w-full h-170
+              className="group flex flex-col gap-4 bg-gray-900 p-4 border border-stone-800 rounded-lg w-full h-160
               transition-all duration-300 transform hover:scale-[1.03] hover:-translate-y-1 hover:shadow-[0_10px_25px_rgba(0,0,0,0.25)] hover:border-lime-400"
             >
               <div className="flex items-center justify-between px-2">
@@ -186,15 +186,15 @@ export default function Input() {
               ) : (
                 <div className="flex flex-col justify-between gap-10 px-1.5 py-4 h-full">
                   {/* Øverste sektion – titel og beskrivelse */}
-                  <div className="flex flex-col gap-6 h-3/6">
-                    <p className="text-xl font-semibold transition-colors duration-300 group-hover:text-lime-400">
+                  <div className="flex flex-col gap-6 h-4/7">
+                    <p className="text-xl font-semibold transition-colors duration-300 group-hover:text-lime-400 h-1/3">
                       {rec.title}
                     </p>
-                    <p className="text-sm">{rec.description.ai_solution}</p>
+                    <p className="text-sm h-2/3">{rec.description.ai_solution}</p>
                   </div>
 
                   {/* Midtersektion – tid, effekt, ROI */}
-                  <div className="h-1/6">
+                  <div className="h-1/7">
                     <hr className="border-t border-gray-500" />
                     <div className="flex items-center justify-between mt-4">
                       <div className="flex flex-col justify-center gap-1">
@@ -219,7 +219,7 @@ export default function Input() {
                   </div>
 
                   {/* Nederste sektion – outcomes */}
-                  <div className="flex flex-col gap-4 h-2/6">
+                  <div className="flex flex-col gap-4 justify-start h-2/7">
                     <ul className="list-disc list-inside mt-1 space-y-1 marker:text-lime-400 text-gray-200 text-sm">
                       {Array.isArray(rec.description.expected_outcome) ? (
                         rec.description.expected_outcome.map(
